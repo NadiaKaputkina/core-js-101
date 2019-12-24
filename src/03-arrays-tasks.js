797402b0b5d7be2bad681c0540966b5440d2f9b3
NadiaKaputkina <nadia.kaputkina@gmail.com>
2019-12-24 17:15:03 +0300

    fix

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -296,7 +296,7 @@ function propagateItemsByPositionIndex(arr) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(arr) {
+function get3TopItems(/* arr */) {
   throw new Error('Not implemented');
 }
 
